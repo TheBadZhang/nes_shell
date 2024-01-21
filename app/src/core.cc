6ca5c9a27b2de9e8172dc2108eee468bd173f4f4
TheBadZhang <thebadzhang@qq.com>
2024-01-21 23:33:10 +0800

    优化了框架结构，增加了更多的动画效果

--- app/src/core.cc
+++ app/src/core.cc
@@ -4,13 +4,13 @@
 // https://github.com/Robert-van-Engelen/tinylisp
 // 尝试添加一个 lisp repl 到项目当中
 
+// 移植 TheBadZhang/ege_animation
+
 // nes 模拟器
 
 // 俄罗斯方块
 
 // 生命游戏
-
-// 贪吃蛇
 
 // 摄像头
 
@@ -44,12 +44,13 @@
 
 // #include "tinyexpr.h"
 #include "hanoi.hpp"
+#include "snake.hpp"
 #include "sound_wave.hpp"
 
 
 extern const unsigned short test_img_128x128[][128];
 
-
+#include "app.hpp"
 
 #include "alert.hpp"
 
@@ -188,8 +189,8 @@ void ips_func(void* argument) {
 	// tbz::tft::st7735::init();
 	// vTaskDelay(100);
 	tbz::tft::st7735::fillScreen(tbz::tft::st7735::COLOR::BLACK);
-	int i = 0;
-	uint16_t c[] { 0x0000,0x001F,0xF800,0x07E0,0x07FF,0xF81F,0xFFE0,0xFFFF };
+	// int i = 0;
+	// uint16_t c[] { 0x0000,0x001F,0xF800,0x07E0,0x07FF,0xF81F,0xFFE0,0xFFFF };
 	while (true) {
 		// 依次使用各种背景颜色
 		// tbz::tft::st7735::fillScreen(c[i++]);
@@ -221,21 +222,17 @@ str ui_list[] = {
 };
 
 
-enum class NOW_SCENE {
+enum class WINDOW {
 	unexist_scene,   // 不存在的场景
-	WELCOM_SCENE, START_SCENE,
-	normal_scene,
-	art_generator,
-	ui_test,
-	ui_test2,
-	ui_test3,
 	fade_animation,
+	layer_in_animation,
+	layer_out_animation,
+	window_start_animation,
 	alert,
-	animation1,
-	animation2,
-	animation3,
-} now_scene = NOW_SCENE::normal_scene, next_scene = NOW_SCENE::unexist_scene;
 
+	normal_status
+
+} now_scene = WINDOW::normal_status, next_scene = WINDOW::unexist_scene;
 
 // u8g2_font_NokiaSmallBold_tf
 /**
@@ -248,21 +245,82 @@ void draw_pic(U8G2* u8g2, uint8_t x, uint8_t y, const uint8_t* pic) {
 		pic2xbmp(pic));
 }
 
+void draw_picture(U8G2* u8g2, int x, int y, const uint8_t* pic) {
+	if (x < 0 || y < 0) {
+		auto h = get_pic_height(pic);
+		auto w = get_pic_width(pic);
+		int blen = (w+7)/8;
+		pic += 2;         // 略过宽高信息
 
+		if (y < 0) {
+			pic = pic + blen*(-y);
+			y = 0;
+		}
 
-
-void fade_to_next_scene(U8G2* u8g2, NOW_SCENE scene) {
-	// swap_animation(u8g2);
-	now_scene = NOW_SCENE::fade_animation;
-	next_scene = scene;
+		// 绘制每一行的像素
+		while (h > 0) {
+			// 绘制一行中的像素
+			for (int i = 0; i < w; i++) {
+				if (i+x < 0) continue;       // 超出屏幕左边界
+				// 读取图片中的像素颜色
+				u8g2 -> setDrawColor(pic[i/8] >> (i%8) & 0x1);
+				u8g2 -> drawPixel(i+x, y);
+			}
+			// 下一行
+			pic += blen;
+			y++;
+			h--;
+		}
+		u8g2 -> setDrawColor(1);  // 恢复默认颜色
+	} else {
+		draw_pic(u8g2, x, y, pic);
+	}
 }
-void next_scene_func(NOW_SCENE scene) {
+
+
+// 这两的顺序必须保持一致才可以
+enum class APP_ENUM {
+	WELCOM_SCENE = 0,
+	key_test,
+	art_generator,
+	ui_test,
+	ui_test2,
+	ui_test3,
+	animation1,
+	animation2,
+	animation3,
+	HANOI_GAME,
+	SNAKE_GAME,
+
+	main
+} now_app = APP_ENUM::main, next_app = APP_ENUM::main;
+
+void fade_to_next_scene(APP_ENUM app) {
+	// swap_animation(u8g2);
+	next_scene = now_scene;
+	now_scene = WINDOW::fade_animation;
+	now_app = app;
+}
+void next_scene_func(WINDOW scene) {
 	now_scene = scene;
 }
 
 
-
-game_hanoi hanoi;
+int tbz::APP::now_select_app_id = 0;
+tbz::APP apps[] {
+	tbz::APP(unknow_app_icon, "WELCOM_SCENE"),
+	tbz::APP(unknow_app_icon, "key_test"),
+	tbz::APP(unknow_app_icon, "art_generator"),
+	tbz::APP(unknow_app_icon, "ui_test"),
+	tbz::APP(unknow_app_icon, "ui_test2"),
+	tbz::APP(unknow_app_icon, "ui_test3"),
+	tbz::APP(unknow_app_icon, "animation1"),
+	tbz::APP(unknow_app_icon, "animation2"),
+	tbz::APP(unknow_app_icon, "animation3"),
+	tbz::APP(hanoi_app_icon,  "HANOI_GAME"),
+	tbz::APP(unknow_app_icon, "SNAKE_GAME"),
+};
+tbz::game::hanoi hanoi;
 
 extern const uint8_t snow_animation_pic[];
 tbz::SPRITE_ANIMATION<10> ani1(snow_animation_pic, 72, 8, 8, 8, 9);
@@ -273,7 +331,7 @@ tbz::round_watch_face rwf;
 tbz::SquareWatch sw;
 
 
-
+tbz::game::SNAKE snake;
 // const char str122[] {"你好，世界"};
 // uint8_t str122_x, str122_y;
 // Create the QR code
@@ -289,6 +347,7 @@ void oled_func(void* argument) {
 	u8g2.begin();
 	// game_hanoi hanoi(&u8g2);
 	hanoi.set_U8G2(&u8g2);
+	snake.set_U8G2(&u8g2);
 	ani1.set_U8G2(&u8g2);
 	ani2.set_U8G2(&u8g2);
 	sw.set_U8G2(&u8g2);
@@ -296,10 +355,12 @@ void oled_func(void* argument) {
 	get_art_index_random();
 	std::hash<const char*> hash_fn;
 
+	// now_scene = static_cast<WINDOW>(0);
+
 	qrcode_initText(&qrcode, qrcodeData, qrcode_version, 0, "HELLO WORLD");
 	// char* sss = (char*)malloc(128);
 
-
+	double ani_n = 0;
 	// ADC 定时采集
 	// HAL_TIM_Base_Start_IT(&htim16);
 	// str122_x = (128-u8g2.getUTF8Width(str122))/2;
@@ -307,193 +368,264 @@ void oled_func(void* argument) {
 	while (true) {
 
 		// u8g2.clearBuffer();
-		// sprintf(buf, "address:%X", sss);
+		// u8g2.setFont(u8g2_font_6x10_tf);
+		// sprintf(buf, "address:%X", 1234);
 		// u8g2.drawStr(0, 10, buf);
+
+
 		if (key_pressed_func(4)) {
 			switch(now_scene) {
 				default: {
 					next_scene = now_scene;
-					now_scene = NOW_SCENE::alert;
+					now_scene = WINDOW::alert;
 				} break;
-				case NOW_SCENE::alert: {
+				case WINDOW::alert: {
 					now_scene = next_scene;
 				}
 			}
 		}
 
-		if (key_pressed_func(10)) {
-			switch (now_scene) {
-				case NOW_SCENE::normal_scene: {
-					fade_to_next_scene(&u8g2, NOW_SCENE::WELCOM_SCENE);
-				} break;
-				case NOW_SCENE::WELCOM_SCENE: {
-					fade_to_next_scene(&u8g2, NOW_SCENE::START_SCENE);
-				} break;
-				case NOW_SCENE::START_SCENE: {
-					fade_to_next_scene(&u8g2, NOW_SCENE::art_generator);
-				} break;
-				case NOW_SCENE::art_generator: {
-					fade_to_next_scene(&u8g2, NOW_SCENE::ui_test);
-				} break;
-				case NOW_SCENE::ui_test: {
-					fade_to_next_scene(&u8g2, NOW_SCENE::ui_test2);
-				} break;
-				case NOW_SCENE::ui_test2: {
-					fade_to_next_scene(&u8g2, NOW_SCENE::ui_test3);
-				} break;
-				case NOW_SCENE::ui_test3: {
-					fade_to_next_scene(&u8g2, NOW_SCENE::animation1);
-				} break;
-				case NOW_SCENE::animation1: {
-					fade_to_next_scene(&u8g2, NOW_SCENE::animation2);
-				} break;
-				case NOW_SCENE::animation2: {
-					fade_to_next_scene(&u8g2, NOW_SCENE::animation3);
-				} break;
-				case NOW_SCENE::animation3: {
-					fade_to_next_scene(&u8g2, NOW_SCENE::normal_scene);
-				} break;
+		if (key_pressed_func(14)) {
+			switch (now_app) {
+				case APP_ENUM::key_test:           fade_to_next_scene(APP_ENUM::WELCOM_SCENE);     break;
+				case APP_ENUM::WELCOM_SCENE:       fade_to_next_scene(APP_ENUM::HANOI_GAME);       break;
+				case APP_ENUM::HANOI_GAME:         fade_to_next_scene(APP_ENUM::SNAKE_GAME);       break;
+				case APP_ENUM::SNAKE_GAME:         fade_to_next_scene(APP_ENUM::art_generator);    break;
+				case APP_ENUM::art_generator:      fade_to_next_scene(APP_ENUM::ui_test);          break;
+				case APP_ENUM::ui_test:            fade_to_next_scene(APP_ENUM::ui_test2);         break;
+				case APP_ENUM::ui_test2:           fade_to_next_scene(APP_ENUM::ui_test3);         break;
+				case APP_ENUM::ui_test3:           fade_to_next_scene(APP_ENUM::animation1);       break;
+				case APP_ENUM::animation1:         fade_to_next_scene(APP_ENUM::animation2);       break;
+				case APP_ENUM::animation2:         fade_to_next_scene(APP_ENUM::animation3);       break;
+				case APP_ENUM::animation3:         fade_to_next_scene(APP_ENUM::key_test);         break;
 			}
 		}
+
+		if (key_pressed_func(9)) {
+			next_app = APP_ENUM::main;
+			fade_to_next_scene(next_app);
+		}
+
 		switch (now_scene) {
-			case NOW_SCENE::ui_test3: {
-				u8g2.drawBox(0,0,20,20);
-				u8g2.drawBox(20,20,20,20);
-				u8g2.sendBuffer();
-				u8g2.drawFrame(10,40,20,20);
-				u8g2.sendBuffer();
-				u8g2.setFont(u8g2_font_DigitalDiscoThin_tf);
-				sprintf(buf,"%d",114514);
-				u8g2.drawStr(0,20,buf);
+			case WINDOW::fade_animation: {
+				tbz::animation::slide(&u8g2, now_scene, next_scene);
 			} break;
-			case NOW_SCENE::ui_test2: {
-				const int screen_width = u8g2.getDisplayWidth();
-				const int screen_height = u8g2.getDisplayHeight();
-				const int screen_size = std::min(screen_width, screen_height);
-
-				auto size = 17+4*qrcode_version;
-				auto scale = screen_size/size;
-
-				auto offset_x = (screen_width-size*scale)/2;
-				auto offset_y = (screen_height-size*scale)/2;
-
-				for (uint8_t y = 0; y < qrcode.size; y++) {
-					for (uint8_t x = 0; x < qrcode.size; x++) {
-						u8g2.setDrawColor(qrcode_getModule(&qrcode, x, y) ? 1 : 0);
-						u8g2.drawBox(offset_x+scale*x, offset_y+scale*y, scale, scale);
-					}
-				}
-				u8g2.setDrawColor(1);
-			} break;
-			case NOW_SCENE::animation1: {
-				alert_font = u8g2_font_wqy16_t_gb2312;
-				sprintf(alert_message, "你好，世界");
-				// 动画1
-				// u8g2.drawUTF8(str122_x, str122_y, str122);
-				ani1.draw();
-				ani2.draw();
-			} break;
-			case NOW_SCENE::animation2: {
-				// 动画2
-				HAL_ADC_Start(&hadc1);
-				HAL_ADC_PollForConversion(&hadc1, 50);
-
-				rwf.draw(HAL_ADC_GetValue(&hadc1)/33.0);
-				// sound_wave.draw_sound_wave();
-			} break;
-			case NOW_SCENE::animation3: {
-				// 动画3
-				if (time_seconds >= 60) {
-					time_seconds = 0;
-					time_minutes++;
-					if (time_minutes >= 60) {
-						time_minutes = 0;
-						time_hours++;
-						if (time_hours >= 12) {
-							time_hours = 0;
-						}
-					}
-				}
-				sw.draw(time_hours, time_minutes, time_seconds);
-			} break;
-			case NOW_SCENE::alert: {
+			// case WINDOW::window_start_animation: {
+			// 	tbz::animation::window_start(&u8g2, now_scene, next_scene);
+			// } break;
+			// case WINDOW::layer_in_animation: {
+			// 	tbz::animation::layer_in(&u8g2, now_scene, next_scene);
+			// } break;
+			// case WINDOW::layer_out_animation: {
+			// 	tbz::animation::layer_out(&u8g2, now_scene, next_scene);
+			// } break;
+			case WINDOW::alert: {
 				tbz::ui::menu::alert::draw(&u8g2);
 			} break;
-			case NOW_SCENE::fade_animation: {
-				tbz::animation::swap_animation(&u8g2, now_scene, next_scene);
-			} break;
-			case NOW_SCENE::START_SCENE: {
-				if (key_pressed_func(1)) {
-					hanoi.cursor_left();
-				}
-				if (key_pressed_func(11)) {
-					hanoi.cursor_right();
-				}
-				if (key_pressed_func(6)) {
-					hanoi.cursor_select();
-				}
-				alert_font = u8g2_font_wqy16_t_gb2312;
-				sprintf(alert_message, "汉诺塔");
-				hanoi.start_scene();
-			} break;
-			case NOW_SCENE::WELCOM_SCENE: {
-				alert_font = u8g2_font_NokiaSmallBold_tf;
-				sprintf(alert_message, "WELCOM TO PLAY!");
-				hanoi.welcom_scene();
-			} break;
-			case NOW_SCENE::art_generator: {
-				alert_font = u8g2_font_wqy16_t_gb2312;
-				sprintf(alert_message, "艺术发生器");
-				u8g2.clearBuffer();
-				if (key_pressed_func(0)) {
-					get_art_index_random();
-				}
-				u8g2.setFont(u8g2_font_wqy16_t_gb2312);
-				sprintf(buf, "%s%s的", verb[aaa], adj[bbb]);
-				u8g2.drawUTF8(0, 16, buf);
-				sprintf(buf, "%s", noun[ccc]);
-				u8g2.drawUTF8(0, 32, buf);
 
-			} break;
-			case NOW_SCENE::ui_test: {
-				u8g2.clearBuffer();
-				u8g2.setFont(u8g2_font_micro_mr);
-				for (int i = 0; i < sizeof(ui_list)/sizeof(str); i++) {
-					u8g2.drawUTF8(0, 6+6*i, ui_list[i]);
-				}
-
-			} break;
-			case NOW_SCENE::normal_scene: {
-				alert_font = u8g2_font_NokiaSmallBold_tf;
-				sprintf(alert_message, "Hello World!");
-				u8g2.clearBuffer();
-				u8g2.setFont(u8g2_font_6x10_tf);
-				// u8g2.setContrast(25);
-				u8g2.drawStr(0, 10, "Hello World!");
-				int pressed_key_count = 0;
-				for (int i = 0; i < 3; i++) {
-					for (int j = 0; j < 5; j++) {
-						sprintf(buf, "%c", 'A'+i*5+j);
-						u8g2.drawStr(17+22*j, 43+22*i, buf);
-						if (key[i*5+j]) {
-							if (pressed_key_count < 6)
-								keyboard[2+pressed_key_count] = 4+5*i+j;
-							pressed_key_count ++;
-							u8g2.drawBox(11+22*j, 31+22*i, 19, 19);
-						} else {
-							keyboard[2+pressed_key_count] = 0;
-							u8g2.drawFrame(10+22*j, 30+22*i, 20, 20);
+			case WINDOW::normal_status: {
+				switch (now_app) {
+					case APP_ENUM::main: {
+						if (key_pressed_func(0)) {
+							tbz::APP::now_select_app_id--;
+							if (tbz::APP::now_select_app_id < 0) {
+								tbz::APP::now_select_app_id = sizeof(apps)/sizeof(tbz::APP)-1;
+								ani_n -= sizeof(apps)/sizeof(tbz::APP)-1;
+							} else {
+								ani_n += 1;
+							}
 						}
-					}
+						if (key_pressed_func(10)) {
+							tbz::APP::now_select_app_id++;
+							if (tbz::APP::now_select_app_id >= sizeof(apps)/sizeof(tbz::APP)) {
+								tbz::APP::now_select_app_id = 0;
+								ani_n += sizeof(apps)/sizeof(tbz::APP)-1;
+							} else {
+								ani_n -= 1;
+							}
+						}
+						if (key_pressed_func(5)) {
+							next_app = static_cast<APP_ENUM>(tbz::APP::now_select_app_id);
+							fade_to_next_scene(next_app);
+						}
+						if (ani_n>0.01) ani_n -= 0.1*(ani_n+0.1);
+						else if(ani_n < -0.01) ani_n += 0.1*(-ani_n+0.1);
+						u8g2.clearBuffer();
+						u8g2.setBitmapMode(1);
+						for (int i = 0; i < sizeof(apps)/sizeof(tbz::APP); i++) {
+							int x = 44+44*(i-tbz::APP::now_select_app_id-ani_n);
+							int y = 44+11*abs(i-tbz::APP::now_select_app_id-ani_n);
+							// int x = 44+44*ani_n+44*(i-tbz::APP::now_select_app_id);
+							// int y = 44+44*ani_n+11*abs(i-tbz::APP::now_select_app_id);
+							if (x < -40 || x > 127) continue;
+							draw_picture(&u8g2, x, y, apps[i].getPic().getBasePic());
+							// draw_pic(&u8g2, x, y, apps[i].getPic().getBasePic());
+						}
+						u8g2.setFont(u8g2_font_wqy16_t_gb2312);
+						u8g2.drawStr(64-u8g2.getUTF8Width(apps[tbz::APP::now_select_app_id].getName())/2, 126, apps[tbz::APP::now_select_app_id].getName());
+					} break;
+					case APP_ENUM::SNAKE_GAME: {
+						if (key_pressed_func(1)) {
+							snake.move_left();
+						}
+						if (key_pressed_func(11)) {
+							snake.move_right();
+						}
+						if (key_pressed_func(6)) {
+							snake.move_down();
+						}
+						if (key_pressed_func(7)) {
+							snake.move_up();
+						}
+						if (key_pressed_func(2)) {
+							snake.speed_down();
+						}
+						if (key_pressed_func(12)) {
+							snake.speed_up();
+						}
+						snake.game();
+					} break;
+					case APP_ENUM::ui_test3: {
+						u8g2.drawBox(0,0,20,20);
+						u8g2.drawBox(20,20,20,20);
+						u8g2.sendBuffer();
+						u8g2.drawFrame(10,40,20,20);
+						u8g2.sendBuffer();
+						u8g2.setFont(u8g2_font_DigitalDiscoThin_tf);
+						sprintf(buf,"%d",114514);
+						u8g2.drawStr(0,20,buf);
+					} break;
+					case APP_ENUM::ui_test2: {
+						const int screen_width = 128;
+						const int screen_height = 128;
+						const int screen_size = std::min(screen_width, screen_height);
+
+						auto size = 17+4*qrcode_version;
+						auto scale = screen_size/size;
+
+						auto offset_x = (screen_width-size*scale)/2;
+						auto offset_y = (screen_height-size*scale)/2;
+
+						for (uint8_t y = 0; y < qrcode.size; y++) {
+							for (uint8_t x = 0; x < qrcode.size; x++) {
+								u8g2.setDrawColor(qrcode_getModule(&qrcode, x, y) ? 1 : 0);
+								u8g2.drawBox(offset_x+scale*x, offset_y+scale*y, scale, scale);
+							}
+						}
+						u8g2.setDrawColor(1);
+					} break;
+					case APP_ENUM::animation1: {
+						alert_font = u8g2_font_wqy16_t_gb2312;
+						sprintf(alert_message, "你好，世界");
+						// 动画1
+						// u8g2.drawUTF8(str122_x, str122_y, str122);
+						ani1.draw();
+						ani2.draw();
+					} break;
+					case APP_ENUM::animation2: {
+						// 动画2
+						HAL_ADC_Start(&hadc1);
+						HAL_ADC_PollForConversion(&hadc1, 50);
+
+						rwf.draw(HAL_ADC_GetValue(&hadc1)/33.0);
+						// sound_wave.draw_sound_wave();
+					} break;
+					case APP_ENUM::animation3: {
+						// 动画3
+						if (time_seconds >= 60) {
+							time_seconds = 0;
+							time_minutes++;
+							if (time_minutes >= 60) {
+								time_minutes = 0;
+								time_hours++;
+								if (time_hours >= 12) {
+									time_hours = 0;
+								}
+							}
+						}
+						sw.draw(time_hours, time_minutes, time_seconds);
+					} break;
+					case APP_ENUM::HANOI_GAME: {
+						if (key_pressed_func(1)) {
+							hanoi.cursor_left();
+						}
+						if (key_pressed_func(11)) {
+							hanoi.cursor_right();
+						}
+						if (key_pressed_func(6)) {
+							hanoi.cursor_select();
+						}
+						alert_font = u8g2_font_wqy16_t_gb2312;
+						sprintf(alert_message, "汉诺塔");
+						hanoi.start_scene();
+					} break;
+					case APP_ENUM::WELCOM_SCENE: {
+						alert_font = u8g2_font_NokiaSmallBold_tf;
+						sprintf(alert_message, "WELCOM TO PLAY!");
+						hanoi.welcom_scene();
+					} break;
+					case APP_ENUM::art_generator: {
+						alert_font = u8g2_font_wqy16_t_gb2312;
+						sprintf(alert_message, "艺术发生器");
+						u8g2.clearBuffer();
+						if (key_pressed_func(0)) {
+							get_art_index_random();
+						}
+						u8g2.setFont(u8g2_font_wqy16_t_gb2312);
+						sprintf(buf, "%s%s的", verb[aaa], adj[bbb]);
+						u8g2.drawUTF8(0, 16, buf);
+						sprintf(buf, "%s", noun[ccc]);
+						u8g2.drawUTF8(0, 32, buf);
+
+					} break;
+					case APP_ENUM::ui_test: {
+						u8g2.clearBuffer();
+						u8g2.setFont(u8g2_font_micro_mr);
+						for (int i = 0; i < sizeof(ui_list)/sizeof(str); i++) {
+							u8g2.drawUTF8(0, 6+6*i, ui_list[i]);
+						}
+
+					} break;
+					case APP_ENUM::key_test: {
+						alert_font = u8g2_font_NokiaSmallBold_tf;
+						sprintf(alert_message, "Hello World!");
+						u8g2.clearBuffer();
+						u8g2.setFont(u8g2_font_6x10_tf);
+						// u8g2.setContrast(25);
+						u8g2.drawStr(0, 10, "Hello World!");
+						int pressed_key_count = 0;
+						for (int i = 0; i < 3; i++) {
+							for (int j = 0; j < 5; j++) {
+								sprintf(buf, "%c", 'A'+i*5+j);
+								u8g2.drawStr(17+22*j, 43+22*i, buf);
+								if (key[i*5+j]) {
+									if (pressed_key_count < 6)
+										keyboard[2+pressed_key_count] = 4+5*i+j;
+									pressed_key_count ++;
+									u8g2.drawBox(11+22*j, 31+22*i, 19, 19);
+								} else {
+									keyboard[2+pressed_key_count] = 0;
+									u8g2.drawFrame(10+22*j, 30+22*i, 20, 20);
+								}
+							}
+						}
+
+						sprintf (buf, "pressed:%d", pressed_key_count);
+						u8g2.drawStr(0, 20, buf);
+						sprintf (buf, "hash:%x", hash_fn("hello world!"));
+						u8g2.drawStr(0, 110, buf);
+
+					} break;
 				}
-
-				sprintf (buf, "pressed:%d", pressed_key_count);
-				u8g2.drawStr(0, 20, buf);
-				sprintf (buf, "hash:%x", hash_fn("hello world!"));
-				u8g2.drawStr(0, 110, buf);
-
 			} break;
+
+
+			// case WINDOW::unexist_scene:
+			// default: {
+			// 	now_scene = WINDOW::normal_status;
+			// } break;
 		}
+
 		u8g2.sendBuffer();
 		vTaskDelay(16);
 	}
